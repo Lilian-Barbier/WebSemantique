91d27efc96d589eee6aa1e14855b3e00f07e27d9
Lilian <lilian.barbierlaffitte@gmail.com>
2021-01-12 17:42:57 +0100

    InitialCommit

---
+++ src/main/java/MainOntology.java
@@ -0,0 +1,12 @@
+import java.util.Iterator;
+
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.ontology.OntModelSpec;
+
+public class MainOntology {
+
+    public static void main(String[] args) {
+        System.out.println("Started");
+    }
+}
